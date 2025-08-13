5580e44133476477748995fde39117751fb99ff0
Luíza Cordeiro <luizafcordeiro@yahoo.com.br>
2025-08-13 08:37:02 -0300

    Cadastro Medicos

---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String CRM, Especialidade especialidade, DadosEndereco endereco ) {
+    
+}
